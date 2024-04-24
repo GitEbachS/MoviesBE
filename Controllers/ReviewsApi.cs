161e94d5e9672f1a43f13817d1258773ee6a9574
Shari Ebach <ebachshari@yahoo.com>
2024-04-23 20:37:22 -0500

    api calls added

--- Controllers/ReviewsApi.cs
+++ Controllers/ReviewsApi.cs
@@ -1,6 +1,55 @@
-﻿namespace MoviesBE.Controllers
+﻿using MoviesBE.Models;
+using Microsoft.EntityFrameworkCore;
+using MoviesBE.Dto;
+
+namespace MoviesBE.Controllers
 {
     public class ReviewsApi
     {
+        public static void Map(WebApplication app)
+        {
+            //get reviews by movieId
+            app.MapGet("/reviews/{movieId}", (MoviesBEDbContext db, int movieId) =>
+            {
+                var reviewList = db.Reviews
+                                    .Where(r => r.MovieId == movieId)
+                                    .OrderByDescending(r => r.DateCreated)
+                                    .Select(r => new
+                                    {
+                                        r.Id,
+                                        AuthorName = db.Users.Where(u => u.Id == r.UserId).Select(u => u.Name)
+                                        .FirstOrDefault(),
+                                        r.MovieId,
+                                        r.Rating,
+                                        r.CommentReview,
+                                        DateCreated = r.DateCreated.ToString("MM/dd/yyyy"),
+                                    })
+                                    .ToList();
+
+                return Results.Ok(reviewList);
+            });
+
+            //get reviews by userId
+            app.MapGet("/reviews/{userId}", (MoviesBEDbContext db, int userId) =>
+            {
+                var reviewList = db.Reviews
+                                    .Where(r => r.UserId == userId)
+                                    .OrderByDescending (r => r.DateCreated)
+                                    .Select(r => new
+                                    {
+                                        r.Id,
+                                        AuthorName = db.Users.Where(u => u.Id == r.UserId).Select(u => u.Name)
+                                        .FirstOrDefault(),
+                                        r.MovieId,
+                                        r.Rating,
+                                        r.CommentReview,
+                                        DateCreated = r.DateCreated.ToString("MM/dd/yyyy"),
+                                    })
+                                    .ToList();
+
+                return Results.Ok(reviewList);
+            });
+
+        }
     }
 }
---
+++ Dto/UpdateUserDto.cs
@@ -0,0 +1,14 @@
+﻿using MoviesBE.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace MoviesBE.Dto
+{
+    public class UpdateUserDto
+    {
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string Image {  get; set; }
+        public string Uid { get; set; }
+    }
+}
