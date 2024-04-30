0c624b6a4732f0a034960f18e427389e1ff68703
MaggieChafee <maggie.chafee@gmail.com>
2024-04-30 18:57:00 -0500

    added get movies by genre Id

--- Controllers/GenresApi.cs
+++ Controllers/GenresApi.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Runtime.Serialization;
+using Microsoft.EntityFrameworkCore;
 using MoviesBE.Models;
 
 namespace MoviesBE.Controllers
@@ -13,6 +14,27 @@ namespace MoviesBE.Controllers
                 return db.Genres.ToList();
             });
 
+            //get movie's by genre
+            app.MapGet("/genres/{id}/movies", (MoviesBEDbContext db, int id) =>
+            {
+                var movies = db.Genres
+                    .Include(m => m.Movies)
+                    .Where(g => g.Id == id)
+                    .Select(m => m.Movies
+                        .Select(m => new { 
+                            m.Id,
+                            m.Title,
+                            m.Genres,
+                            m.Image
+                        }))
+                    .ToList();
+                if (movies == null)
+                {
+                    return Results.Empty;
+                }
+
+                return Results.Ok(movies);
+            });
         }
     }
 }
